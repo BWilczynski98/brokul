2513d73513524b6a13a27e73a02cc223f2cef899
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2024-02-07 14:13:22 +0100

    Init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+</template>
